5617b3d970f4f176183a20da09d178fa1eae4b1c
shekharsuman2011 <53559908+shekharsuman2011@users.noreply.github.com>
2020-05-28 01:52:56 +0530

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,7 +1,7 @@
 
 
 (function() {
-    var names = ["Shekhar", "Girja", "Rakesh", "pritam", "raj", "anshu"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
